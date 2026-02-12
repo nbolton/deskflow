69813a3925b5d28dfe080eb5f48b1e16ff5c9dda
nbolton <nick@symless.com>
2026-02-12 13:20:26 +0000

    Deploying to gh-pages from @ nbolton/deskflow@68f867b9144a66814362bed44b15f1f618c86a30 🚀

---
+++ navtreedata.js
@@ -0,0 +1,158 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Deskflow", "index.html", [
+    [ "Building Deskflow", "md_doc_2dev_2build.html", [
+      [ "Configuration", "md_doc_2dev_2build.html#autotoc_md1", [
+        [ "Windows Configuration", "md_doc_2dev_2build.html#autotoc_md2", [
+          [ "Windows and Qt", "md_doc_2dev_2build.html#autotoc_md3", [
+            [ "System Qt", "md_doc_2dev_2build.html#autotoc_md4", null ],
+            [ "vcpkg managed Qt", "md_doc_2dev_2build.html#autotoc_md5", null ]
+          ] ]
+        ] ],
+        [ "macOS codesign", "md_doc_2dev_2build.html#autotoc_md6", null ]
+      ] ],
+      [ "Build", "md_doc_2dev_2build.html#autotoc_md7", null ],
+      [ "Install", "md_doc_2dev_2build.html#autotoc_md8", null ],
+      [ "Making Deskflow packages", "md_doc_2dev_2build.html#autotoc_md9", null ]
+    ] ],
+    [ "Contributing to Deskflow", "contributing_guide.html", [
+      [ "Read the Full Guidelines", "contributing_guide.html#autotoc_md10", null ]
+    ] ],
+    [ "Protocol Reference", "protocol_reference.html", [
+      [ "Protocol Overview", "protocol_reference.html#autotoc_md13", [
+        [ "Key Implementation Files", "protocol_reference.html#autotoc_md14", null ]
+      ] ],
+      [ "Protocol Architecture", "protocol_reference.html#autotoc_md15", null ],
+      [ "Protocol State Machine", "protocol_reference.html#autotoc_md16", [
+        [ "State Descriptions", "protocol_reference.html#autotoc_md17", null ]
+      ] ],
+      [ "Message Categories", "protocol_reference.html#autotoc_md18", null ],
+      [ "Message Reference Table", "protocol_reference.html#autotoc_md19", null ],
+      [ "Typical Control Flow", "protocol_reference.html#autotoc_md20", null ],
+      [ "Protocol Constraints", "protocol_reference.html#autotoc_md21", [
+        [ "Message and Data Size Limits", "protocol_reference.html#autotoc_md22", null ],
+        [ "TLS Handshake and Security (Protocol v1.4+)", "protocol_reference.html#autotoc_md23", null ],
+        [ "Key Code and Modifier Mapping", "protocol_reference.html#autotoc_md24", null ]
+      ] ],
+      [ "Timing and Synchronization", "protocol_reference.html#autotoc_md25", [
+        [ "Keep-Alive Mechanism (Protocol v1.3+)", "protocol_reference.html#autotoc_md26", null ],
+        [ "Synchronization on Screen Entry", "protocol_reference.html#autotoc_md27", null ],
+        [ "Handshake Timeout", "protocol_reference.html#autotoc_md28", null ]
+      ] ],
+      [ "Version Compatibility", "protocol_reference.html#autotoc_md29", [
+        [ "Version Migration Guide", "protocol_reference.html#autotoc_md30", null ]
+      ] ],
+      [ "Implementation Examples", "protocol_reference.html#autotoc_md31", [
+        [ "Connection Lifecycle", "protocol_reference.html#autotoc_md32", null ],
+        [ "Message Handling", "protocol_reference.html#autotoc_md33", null ],
+        [ "Complete Message Exchange Sequence", "protocol_reference.html#autotoc_md34", null ]
+      ] ],
+      [ "Debugging and Troubleshooting", "protocol_reference.html#autotoc_md35", [
+        [ "Common Issues", "protocol_reference.html#autotoc_md36", null ],
+        [ "Debug Tools", "protocol_reference.html#autotoc_md37", null ]
+      ] ],
+      [ "Platform-Specific Implementations", "protocol_reference.html#autotoc_md38", null ],
+      [ "Implementation Checklist", "protocol_reference.html#autotoc_md39", [
+        [ "Basic Client Implementation", "protocol_reference.html#autotoc_md40", null ],
+        [ "Advanced Features", "protocol_reference.html#autotoc_md41", null ]
+      ] ],
+      [ "Reference Implementation", "protocol_reference.html#autotoc_md42", null ],
+      [ "Contributing", "protocol_reference.html#autotoc_md43", null ],
+      [ "Support and Resources", "protocol_reference.html#autotoc_md44", null ]
+    ] ],
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "Topics", "topics.html", "topics" ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Symbols", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", "globals_vars" ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"AboutDialog_8cpp.html",
+"DirectionTypes_8h.html#a1702cf626a6f3bc79a7db1382e2fbba9",
+"KeyTypes_8h.html#a46479cae53a6eb6ee0e891518666ac12",
+"MSWindowsDesks_8cpp.html#a8ff6dc91b2706c1be762576867b4da7d",
+"OSXKeyState_8cpp.html#a5c181c8c92422e7428a02f3ceee556c0",
+"SpacesValidator_8h_source.html",
+"classAppUtilUnix.html#a38bb7cb0ef62a92541337051b103cd82",
+"classArchNetworkWinsock.html#a82599cd8f917122c330252b700600154",
+"classClientProxy1__3.html#a02b20bb30b235e9b6b1d23d4b74d853a",
+"classIArchDaemon.html#aad014dbecf0641b2873dc9f6fd86c93b",
+"classIPlatformScreen.html#aac5b540eb1ee7f2934b2829d2c333b3b",
+"classKeySequenceWidget.html",
+"classMSWindowsScreen.html#a9d102cc1b8c4971f83c007d0649f8784",
+"classPlatformScreen.html#a093a4c52fa3287d575986c88d66278b1",
+"classScreenUnavailableException.html#a14e6003da282369f0f6ce48d25ed9227",
+"classSimpleEventQueueBuffer.html#a2c01276cbb3afc0a8772e967e6da5d0f",
+"classWlClipboard.html#aa6d3bb80ead321708116ac1bf6427bd6",
+"classXWindowsUtil.html#aa225a3a10a43d139294773550eb65840",
+"classdeskflow_1_1KeyMap.html#aef496d7565922670d636e590040eedef",
+"classdeskflow_1_1gui_1_1CoreProcess.html#adab780bcb932ba53c90fc6f6b1f25467",
+"classdeskflow_1_1server_1_1ConfigReadContext.html#a015b02335b17c35ccbb460687c2b9dea",
+"functions_func_l.html",
+"namespacedeskflow.html#a34787a880fd439e02383f336665d7bc2a786d0034ff93ad270bacde98d603b3e9",
+"structSettings_1_1Client.html#aa09822d6262ecd66e109aa7ab4f46d84"
+];
+
+var SYNCONMSG = 'click to disable panel synchronization';
+var SYNCOFFMSG = 'click to enable panel synchronization';
+var LISTOFALLMEMBERS = 'List of all members';
---
+++ search/defines_2.js
@@ -0,0 +1,15 @@
+var searchData=
+[
+  ['clog_0',['CLOG',['../Log_8h.html#a05785244b5afab2f72b74f922dfb777b',1,'Log.h']]],
+  ['clog_5fcrit_1',['CLOG_CRIT',['../Log_8h.html#ae540a914cab70d94fb4963a03cd4f363',1,'Log.h']]],
+  ['clog_5fdebug_2',['CLOG_DEBUG',['../Log_8h.html#a3dc6b9e7273bc09396310d9cdf41f690',1,'Log.h']]],
+  ['clog_5fdebug1_3',['CLOG_DEBUG1',['../Log_8h.html#ac52a509bb9cd20414ba728bb1a0dc373',1,'Log.h']]],
+  ['clog_5fdebug2_4',['CLOG_DEBUG2',['../Log_8h.html#a1a6f5d9796ad6b8fcd42f27dd6518b73',1,'Log.h']]],
+  ['clog_5ferr_5',['CLOG_ERR',['../Log_8h.html#ac2c0dbe55ad449a4f4ea00e1f5a93160',1,'Log.h']]],
+  ['clog_5finfo_6',['CLOG_INFO',['../Log_8h.html#a984c30f63a9501d242fedd0439f1ed11',1,'Log.h']]],
+  ['clog_5fipc_7',['CLOG_IPC',['../Log_8h.html#a7a0d2db99205e9a1d5a6df741fbc1d91',1,'Log.h']]],
+  ['clog_5fnote_8',['CLOG_NOTE',['../Log_8h.html#aac036dc6ac96a54a7db0ff29e61c6e86',1,'Log.h']]],
+  ['clog_5fprint_9',['CLOG_PRINT',['../Log_8h.html#a1cdffb99e278d39c01456433dbd7c55a',1,'Log.h']]],
+  ['clog_5ftrace_10',['CLOG_TRACE',['../Log_8h.html#ade00c678de791b27ba8ee5ee4ca6b697',1,'Log.h']]],
+  ['clog_5fwarn_11',['CLOG_WARN',['../Log_8h.html#aa35f4018af5f6bc5a8e9a5da56823b94',1,'Log.h']]]
+];
